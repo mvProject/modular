87d9f05d5523fb06f8503319410e39b3be4067fb
mvproject <awsom7@gmail.com>
2021-08-29 16:02:44 +0300

    convert app gradle

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -16,7 +16,6 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
     }
 
     buildTypes {
@@ -28,10 +27,12 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
